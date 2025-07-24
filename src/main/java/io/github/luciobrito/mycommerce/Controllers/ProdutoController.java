635fbcde945d62d3c452d0e6fc2254252f4b178d
luciobrito <luciobp869@gmail.com>
2025-07-24 01:08:05 -0300

    Validação no Controller do Produto

--- src/main/java/io/github/luciobrito/mycommerce/Controllers/ProdutoController.java
+++ src/main/java/io/github/luciobrito/mycommerce/Controllers/ProdutoController.java
@@ -3,13 +3,17 @@ package io.github.luciobrito.mycommerce.Controllers;
 import io.github.luciobrito.mycommerce.DTOs.ProdutoDTO;
 import io.github.luciobrito.mycommerce.Models.Produto;
 import io.github.luciobrito.mycommerce.Services.ProdutoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 //Provisório, remover depois
 @CrossOrigin
@@ -22,8 +26,17 @@ public class ProdutoController {
         return produtoService.all();
     }
     @PostMapping("/produto")
-    public ResponseEntity<Produto> CadastrarProduto(@RequestBody ProdutoDTO produtoDTO){
+    public ResponseEntity<?> CadastrarProduto(@RequestBody @Valid ProdutoDTO produtoDTO, BindingResult bindingResult){
         Produto produto = new Produto();
+        Map<String,String> validationObj = new HashMap<String,String>();
+
+        if(bindingResult.hasErrors()){
+            bindingResult.getFieldErrors().forEach(e -> {
+                validationObj.put(e.getField(),e.getDefaultMessage());
+            });
+            return ResponseEntity.badRequest().body(validationObj);
+        }
+        //Produto.class.accessFlags().forEach(x->{x.name();});
         BeanUtils.copyProperties(produtoDTO, produto);
         return ResponseEntity.status(HttpStatus.CREATED).body(produtoService.cadastrar(produto));
     }
--- src/main/java/io/github/luciobrito/mycommerce/DTOs/ProdutoDTO.java
+++ src/main/java/io/github/luciobrito/mycommerce/DTOs/ProdutoDTO.java
@@ -1,5 +1,9 @@
 package io.github.luciobrito.mycommerce.DTOs;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-public record ProdutoDTO (String nome, String descricao, Double preco, @Column(name = "codigo_barra") String codigoBarra) { }
+public record ProdutoDTO (@NotBlank(message = "Nome não pode estar vazio") @NotNull(message = "Nome não pode ser nulo") String nome, String descricao, @Min(value = 0, message = "Valor não pode ser menor que 0") Double preco, @Column(name = "codigo_barra") @Size(min = 3, max = 13)  String codigoBarra) { }
